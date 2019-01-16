6dfcd3b5d13c40528c99979f424742e7e8d2de62
h2o <h2o_zs@163.com>
2019-01-16 18:11:26 +0800

    刚添完数据,马上话题列表页面

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::resource('users', 'UsersController', ['only'=>['show', 'edit', 'update']]
 // Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
